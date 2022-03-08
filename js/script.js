9c2d6543ab9a632006eb5277a72d4e443187d7b0
Eli Mitchell <985041+cyberstream@users.noreply.github.com>
2022-03-07 22:14:49 -0900

    Update per AirBnB style guide

--- js/script.js
+++ js/script.js
@@ -1,138 +1,138 @@
+/* global document */
+
 // computerPlay generates a random choice of rock, paper, or scissors.
 
 function computerPlay() {
-    let options = ["rock", "paper", "scissors"];
-    let randomIndex = Math.floor(Math.random() * options.length);
-    
-    return options[randomIndex];
-}
+  const options = ['rock', 'paper', 'scissors'];
+  const randomIndex = Math.floor(Math.random() * options.length);
 
-/* 
-    playRound takes choices from two players, compares the results, 
-    and returns a string with the results.
-
-    Input must be a valid choice (use isValidChoice to validate). 
-    Second argument defaults to computer's random choice. 
-*/
-
-function playRound(playerSelection, computerSelection = computerPlay()) {
-    playerSelection = cleanInput(playerSelection);
-
-    // Generate capitalized versions of selections
-    upperPlayerSelection =  playerSelection[0].toUpperCase() + 
-                                        playerSelection.substring(1);
-
-    upperComputerSelection =  computerSelection[0].toUpperCase() + 
-                                        computerSelection.substring(1);
-
-    // Possible results for player: win, lose, tie. 
-    let results = {
-        win: `You win! ${upperPlayerSelection} beats ${computerSelection}!`,
-        lose: `You lose! ${upperComputerSelection} beats ${playerSelection}!`,
-        tie: `You tie! Both selected ${playerSelection}! Try again.`
-    }
-
-    // If options are same, return TIE
-    if (playerSelection == computerSelection) {
-        return results.tie;
-    }
-
-    // Check for all winning possibilities. If true, return WIN.
-    else if (playerSelection == 'rock' && computerSelection == 'scissors' || 
-                    playerSelection == 'paper' && computerSelection == 'rock' || 
-                    playerSelection == 'scissors' && computerSelection == 'paper') {      
-        return results.win;
-    }
-
-    // Else return LOSE
-    else return results.lose;
+  return options[randomIndex];
 }
 
 // Takes input of any data type and returns lowercase string
 
 function cleanInput(userInput) {
-    return (String(userInput)).toLowerCase();
+  return (String(userInput)).toLowerCase();
 }
 
-/* 
-    Checks if provided value (any data type) is a valid option for the game. 
+/*
+    playRound takes choices from two players, compares the results,
+    and returns a string with the results.
+
+    Input must be a valid choice (use isValidChoice to validate).
+    Second argument defaults to computer's random choice.
+*/
+
+function playRound(rawPlayerSelection, computerSelection = computerPlay()) {
+  const playerSelection = cleanInput(rawPlayerSelection);
+
+  // Generate capitalized versions of selections
+  const upperPlayerSelection = playerSelection[0].toUpperCase()
+                                  + playerSelection.substring(1);
+
+  const upperComputerSelection = computerSelection[0].toUpperCase()
+                                    + computerSelection.substring(1);
+
+  // Possible results for player: win, lose, tie.
+  const results = {
+    win: `You win! ${upperPlayerSelection} beats ${computerSelection}!`,
+    lose: `You lose! ${upperComputerSelection} beats ${playerSelection}!`,
+    tie: `You tie! Both selected ${playerSelection}! Try again.`,
+  };
+
+  // If options are same, return TIE
+  if (playerSelection === computerSelection) {
+    return results.tie;
+  }
+
+  if ((playerSelection === 'rock' && computerSelection === 'scissors')
+          || (playerSelection === 'paper' && computerSelection === 'rock')
+          || (playerSelection === 'scissors' && computerSelection === 'paper')) {
+    return results.win;
+  }
+
+  return results.lose;
+}
+
+/*
+    Checks if provided value (any data type) is a valid option for the game.
     Returns true if choice is valid; false otherwise.
 */
 
-function isValidChoice(choice) {
-    // Array of possible options: rock, paper, scissors
-    let options = ["rock", "paper", "scissors"];
+function isValidChoice(rawChoice) {
+  // Array of possible options: rock, paper, scissors
+  const options = ['rock', 'paper', 'scissors'];
 
-    // Convert parameter to lowercase
-    choice = cleanInput(choice)
+  // Convert parameter to lowercase
+  const choice = cleanInput(rawChoice);
 
-    // Check if parameter is in list of valid options
-    result = options.includes(choice);
-
-    return result;
+  // Return boolean for whether parameter is in list of valid options
+  return options.includes(choice);
 }
 
-/* 
-    Gets user input via prompt window. 
-    Will continue to re-prompt if invalid value is entered. 
+/*
+    Gets user input via prompt window.
+    Will continue to re-prompt if invalid value is entered.
     Will return string of value once valid choice is entered.
 */
 
 function getValidChoice() {
-    // No valid choice has been entered yet
-    let valid = false;
+  // No valid choice has been entered yet
+  const valid = false;
 
-    // Default prompt message
-    let promptMsg = 'Please enter your choice: rock, paper, or scissors.';
+  // Default prompt message
+  let promptMsg = 'Please enter your choice: rock, paper, or scissors.';
 
-    // Keeping trying to get user input until 
-    while (!valid) {
-        // Ask user for input
-        let userChoice = prompt(promptMsg);
+  // Keeping trying to get user input until
+  while (!valid) {
+    // Ask user for input
+    let userChoice = window.prompt(promptMsg);
 
-        // Convert to lowercase string
-        userChoice = cleanInput(userChoice);
+    // Convert to lowercase string
+    userChoice = cleanInput(userChoice);
 
-        /*  
-            Check if choice is valid. 
-            If it is, return the value. 
+    /*
+            Check if choice is valid.
+            If it is, return the value.
             If it isn't, show error message and try again.
         */
 
-        if (isValidChoice(userChoice)) return userChoice;
+    if (isValidChoice(userChoice)) return userChoice;
 
-        else promptMsg = `"${userChoice}" is not a valid choice. \r\n\r\n` +
-                                'Please try again using one of these choices:' +
-                                ' rock, paper, or scissors.';
-    }  
+    promptMsg = `"${userChoice}" is not a valid choice. \r\n\r\n`
+                                + 'Please try again using one of these choices:'
+                                + ' rock, paper, or scissors.';
+  }
+
+  return false;
 }
 
-// Plays "Rock, Paper, Scissors" game totalRounds times. 
+// Plays "Rock, Paper, Scissors" game totalRounds times.
 
 function game() {
-    // Play game totalRounds times
-    const totalRounds = 5;
+  // Play game totalRounds times
+  const totalRounds = 5;
 
-    for (let i = 0; i < totalRounds; i++) {
-        // Show current round number
-        console.log(`Round ${i + 1} of ${totalRounds}...`);
+  for (let i = 0; i < totalRounds; i++) {
+    // Show current round number
+    console.log(`Round ${i + 1} of ${totalRounds}...`);
 
-        // Get player's choice
-        let playerChoice = getValidChoice();
+    // Get player's choice
+    const playerChoice = getValidChoice();
 
-        // Check against computer's choice
-        let roundResult = playRound(playerChoice);
+    // Check against computer's choice
+    const roundResult = playRound(playerChoice);
 
-        // Announce result
-        console.log(roundResult);
+    // Announce result
+    console.log(roundResult);
 
-        // Insert blank line between rounds
-        console.log('');
-    }
+    // Insert blank line between rounds
+    console.log('');
+  }
 }
 
-// Play game when button is pressed. 
-button = document.getElementById('startGame');
-window.addEventListener('click', () => {
-    game();
+// Play game when button is pressed.
+const button = document.getElementById('startGame');
+button.addEventListener('click', () => {
+  game();
 }, false);
